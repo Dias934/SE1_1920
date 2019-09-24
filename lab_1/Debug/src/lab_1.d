0006ffdf2799403ac2373254d3e1fe27526332b2
Manuel Dias <dias934@gmail.com>
2019-09-24 23:43:32 +0100

    Workbench Setup

---
+++ lab_1/Debug/src/lab_1.d
@@ -0,0 +1,16 @@
+src/lab_1.o src/lab_1.d: ../src/lab_1.c \
+ D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/LPC17xx.h \
+ D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/core_cm3.h \
+ D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/core_cmInstr.h \
+ D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/core_cmFunc.h \
+ D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/system_LPC17xx.h
+
+D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/LPC17xx.h:
+
+D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/core_cm3.h:
+
+D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/core_cmInstr.h:
+
+D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/core_cmFunc.h:
+
+D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/system_LPC17xx.h:
---
+++ project/Debug/src/project.d
@@ -0,0 +1,16 @@
+src/project.o src/project.d: ../src/project.c \
+ D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/LPC17xx.h \
+ D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/core_cm3.h \
+ D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/core_cmInstr.h \
+ D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/core_cmFunc.h \
+ D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/system_LPC17xx.h
+
+D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/LPC17xx.h:
+
+D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/core_cm3.h:
+
+D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/core_cmInstr.h:
+
+D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/core_cmFunc.h:
+
+D:\Isel\19-20\Inverno\SE1\CMSIS_CORE_LPC17xx\inc/system_LPC17xx.h:
